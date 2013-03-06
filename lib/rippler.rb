2463fd2c16adac7f4520feac610060a59f299dca
arvicco <arvicco@gmail.com>
2013-03-05 18:01:59 -0800

    Minimalist script to test WS functionality

--- lib/rippler.rb
+++ lib/rippler.rb
@@ -1,5 +1,36 @@
 require "rippler/version"
+require 'faye/websocket'
+require 'eventmachine'
+require "json"
+require "pp"
 
 module Rippler
-  # Your code goes here...
+
+  def self.run
+
+    EM.run {
+      ws = Faye::WebSocket::Client.new('wss://s1.ripple.com:51233')
+
+      ws.onopen = lambda do |event|
+        p :open
+        ws.send({"command"=>"account_info","ident"=>"rnZoUopPFXRSVGdeDkgbqdft8SbXfJxKYh"}.to_json)
+      end
+
+      ws.onmessage = lambda do |event|
+        p :message
+        pp JSON.parse(event.data)
+        ws.close
+      end
+
+      ws.onerror = lambda do |event|
+        p [:message, event]
+      end
+
+      ws.onclose = lambda do |event|
+        p [:close, event.code, event.reason]
+        ws = nil
+        EM.stop
+      end
+    }
+  end
 end
