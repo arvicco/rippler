96daf4982630283bd60896d248001f2d2f7bd5b4
arvicco <arvicco@gmail.com>
2013-03-12 13:22:05 -0700

    Account from ('account_info') Hash

--- lib/rippler/account.rb
+++ lib/rippler/account.rb
@@ -2,12 +2,30 @@ module Rippler
   class Account
     include Rippler::Utils
 
-    attr_accessor :address
+    attr_accessor :address, :balance, :flags
 
     # String: "evoorhees" or "rpfxDFsjDNtzSBCALKuWoWMkpRp4vxvgrG"
-    def initialize name_or_address
-      @address = Rippler::Contacts[name_or_address] || name_or_address
-      @name = Rippler::Addresses[@address]
+    # Hash:
+    # {"Account"=>"rUvpjNBcnt4DLKEVA2XyvJi1YGCfp2EPtk",
+    #  "Balance"=>"199999990",
+    #  "Flags"=>0,
+    #  "LedgerEntryType"=>"AccountRoot",
+    #  "OwnerCount"=>0,
+    #  "PreviousTxnID"=>"14DBB4EBF6BE71439CE776446337A7E789EB4205D55F1731622DD225AF950BE2",
+    #  "PreviousTxnLgrSeq"=>338219,
+    #  "Sequence"=>2,
+    #  "index"=>"71EE27781DE11E4D3E44052E1264C9F5E01F3F2BC0F5D9A890099914C5031C91"}
+    def initialize data
+      case data
+      when String
+        @address = Rippler::Contacts[data] || data
+        @name = Rippler::Addresses[@address]
+      when Hash
+        @address = data["Account"]
+        @name = Rippler::Addresses[@address]
+        @balance = Money(data["Balance"])
+        @flags = data["Flags"]
+      end
     end
 
     def name
