24250f40a9de3d7e5eee157d170047cb7c434f94
arvicco <arvicco@gmail.com>
2013-03-26 08:37:03 -0700

    Transactions can be sorted by timestamp now

--- lib/rippler/transaction.rb
+++ lib/rippler/transaction.rb
@@ -2,15 +2,26 @@ module Rippler
   # Represents a single Ripple transaction
   class Transaction
     include Rippler::Utils
+    include Comparable
 
     def initialize data # data Hash
       @data = data
     end
 
-    def to_s
-      tx = @data['tx'] || @data['transaction']
+    # Pure transaction data, without metadata
+    def tx
+      @data['tx'] || @data['transaction']
+    end
+
+    def timestring
       if tx
-        "#{Time(tx['date']).strftime("%Y-%m-%d %H:%M:%S")} " +
+        "#{Time(tx['date']).strftime("%Y-%m-%d %H:%M:%S")} "
+      end
+    end
+
+    def to_s
+      if tx
+        timestring +
         case tx["TransactionType"]
         when "Payment"
           "PAY #{Money(tx['Amount'])} #{Account(tx['Account'])} > #{Account(tx['Destination'])}"
@@ -30,5 +41,9 @@ module Rippler
         @data
       end
     end
+  end
+
+  def <=> other
+    timestring <=> other.timestring
   end
 end
